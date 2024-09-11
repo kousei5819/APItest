fba767085000c1c5d48abd6390dba1737a1631bf
kousei5819 <82437018+kousei5819@users.noreply.github.com>
2024-09-11 17:25:27 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import "https://deno.land/std/dotenv/load.ts";
 /* サーバーにアクセス（fetch）があったら呼び出される関数 */
 Deno.serve(async (req) => {
   const apiKey = Deno.env.get("API_KEY"); // 環境変数の読み込み
-  console.log(apiKey); // ここはVSCodeのターミナル
 
   // メッセージボディを構成
   const body = JSON.stringify({ env: apiKey });
--- script.js
+++ script.js
@@ -4,8 +4,6 @@ async function getResource() {
   // const res = await fetch("http://localhost:8000/");
   const res = await fetch("https://kousei5819-apitest-30.deno.dev/");
   const obj = await res.json();
-  console.log(obj); // ここはブラウザーの開発者ツールのコンソール
-  document.querySelector("h1").textContent = obj.env;
 
   const { createClient } = microcms;
 
