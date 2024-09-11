c13533e1d75444bb7c83409ee23f9fde8e8be05c
kousei5819 <82437018+kousei5819@users.noreply.github.com>
2024-09-11 16:46:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+/* サーバーに接続する非同期関数 */
+async function getResource() {
+  // クライアントサイドからサーバーサイドへはfetchで接続する
+  // const res = await fetch("http://localhost:8000/");
+  const res = await fetch("https://kousei5819-apitest-30.deno.dev/");
+  const obj = await res.json();
+  console.log(obj); // ここはブラウザーの開発者ツールのコンソール
+  document.querySelector("h1").textContent = obj.env;
+}
+
+// button要素のclickイベントに登録
+document.querySelector("button").addEventListener("click", getResource);
+
 const { createClient } = microcms;
 
 // Initialize Client SDK.
@@ -25,16 +38,3 @@ client
       memberItem.appendChild(li);
     });
   });
-
-/* サーバーに接続する非同期関数 */
-async function getResource() {
-  // クライアントサイドからサーバーサイドへはfetchで接続する
-  // const res = await fetch("http://localhost:8000/");
-  const res = await fetch("https://kousei5819-apitest-30.deno.dev/");
-  const obj = await res.json();
-  console.log(obj); // ここはブラウザーの開発者ツールのコンソール
-  document.querySelector("h1").textContent = obj.env;
-}
-
-// button要素のclickイベントに登録
-document.querySelector("button").addEventListener("click", getResource);
