ffca4229488f352192338f5b23bdd94b828232fb
kousei5819 <82437018+kousei5819@users.noreply.github.com>
2024-09-11 17:08:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,35 +6,35 @@ async function getResource() {
   const obj = await res.json();
   console.log(obj); // ここはブラウザーの開発者ツールのコンソール
   document.querySelector("h1").textContent = obj.env;
+
+  const { createClient } = microcms;
+
+  // Initialize Client SDK.
+  const client = createClient({
+    serviceDomain: "p5aur2gmg1", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+    apiKey: obj.env,
+    // retry: true // Retry attempts up to a maximum of two times.
+  });
+
+  client
+    .get({
+      endpoint: "member",
+    })
+    .then((res) => {
+      // console.log(res)
+      const memberItem = document.querySelector("#memberItem");
+      res.contents.forEach((content) => {
+        const li = document.createElement("li");
+        const div = document.createElement("div");
+        div.innerHTML = content.body;
+        const p = document.createElement("h3");
+        p.textContent = content.title;
+        li.appendChild(p);
+        li.appendChild(div);
+        memberItem.appendChild(li);
+      });
+    });
 }
 
 // button要素のclickイベントに登録
 document.querySelector("button").addEventListener("click", getResource);
-
-const { createClient } = microcms;
-
-// Initialize Client SDK.
-const client = createClient({
-  serviceDomain: "p5aur2gmg1", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: obj,
-  // retry: true // Retry attempts up to a maximum of two times.
-});
-
-client
-  .get({
-    endpoint: "member",
-  })
-  .then((res) => {
-    // console.log(res)
-    const memberItem = document.querySelector("#memberItem");
-    res.contents.forEach((content) => {
-      const li = document.createElement("li");
-      const div = document.createElement("div");
-      div.innerHTML = content.body;
-      const p = document.createElement("h3");
-      p.textContent = content.title;
-      li.appendChild(p);
-      li.appendChild(div);
-      memberItem.appendChild(li);
-    });
-  });
