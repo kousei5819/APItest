e6a3d11305ea9b46b657aa60ed7526f14c7e91a5
kousei5819 <82437018+kousei5819@users.noreply.github.com>
2024-09-11 16:54:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import "https://deno.land/std/dotenv/load.ts";
 
 /* サーバーにアクセス（fetch）があったら呼び出される関数 */
 Deno.serve(async (req) => {
-  const apiKey = Deno.env.get("API_KEY"); // 環境変数の読み込み
-  console.log(apiKey); // ここはVSCodeのターミナル
+  //   const apiKey = Deno.env.get("API_KEY"); // 環境変数の読み込み
+  //   console.log(apiKey); // ここはVSCodeのターミナル
 
   // メッセージボディを構成
   const body = JSON.stringify({ env: apiKey });
@@ -16,3 +16,31 @@ Deno.serve(async (req) => {
     },
   });
 });
+
+const { createClient } = microcms;
+
+// Initialize Client SDK.
+const client = createClient({
+  serviceDomain: "p5aur2gmg1", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+  apiKey: Deno.env.get("API_KEY"),
+  // retry: true // Retry attempts up to a maximum of two times.
+});
+
+client
+  .get({
+    endpoint: "member",
+  })
+  .then((res) => {
+    // console.log(res)
+    const memberItem = document.querySelector("#memberItem");
+    res.contents.forEach((content) => {
+      const li = document.createElement("li");
+      const div = document.createElement("div");
+      div.innerHTML = content.body;
+      const p = document.createElement("h3");
+      p.textContent = content.title;
+      li.appendChild(p);
+      li.appendChild(div);
+      memberItem.appendChild(li);
+    });
+  });
--- script.js
+++ script.js
@@ -10,32 +10,3 @@ async function getResource() {
 
 // button要素のclickイベントに登録
 document.querySelector("button").addEventListener("click", getResource);
-
-const { createClient } = microcms;
-
-const apiKey = Deno.env.get("API_KEY");
-// Initialize Client SDK.
-const client = createClient({
-  serviceDomain: "p5aur2gmg1", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: apiKey,
-  // retry: true // Retry attempts up to a maximum of two times.
-});
-
-client
-  .get({
-    endpoint: "member",
-  })
-  .then((res) => {
-    // console.log(res)
-    const memberItem = document.querySelector("#memberItem");
-    res.contents.forEach((content) => {
-      const li = document.createElement("li");
-      const div = document.createElement("div");
-      div.innerHTML = content.body;
-      const p = document.createElement("h3");
-      p.textContent = content.title;
-      li.appendChild(p);
-      li.appendChild(div);
-      memberItem.appendChild(li);
-    });
-  });
