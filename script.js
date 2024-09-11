690fef8fed7ba802ab5b028e808617c4c3f6c708
kousei5819 <82437018+kousei5819@users.noreply.github.com>
2024-09-11 16:38:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ client
 /* サーバーに接続する非同期関数 */
 async function getResource() {
   // クライアントサイドからサーバーサイドへはfetchで接続する
-  const res = await fetch("http://localhost:8000/");
+  // const res = await fetch("http://localhost:8000/");
+  const res = await fetch("https://kousei5819-apitest-30.deno.dev");
   const obj = await res.json();
   console.log(obj); // ここはブラウザーの開発者ツールのコンソール
   document.querySelector("h1").textContent = obj.env;
